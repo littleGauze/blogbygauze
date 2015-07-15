2ab597c733c73ad49c24b4d036d50b1a94d53273
NealLi <810483824@qq.com>
2015-07-15 17:39:57 +0800

    'modify'

--- routes/myinfos.js
+++ routes/myinfos.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.render('myinfos', {url: req.url, type: 'posted'});
+	res.render('myinfos', {url: req.originalUrl, type: 'posted'});
 });
 
 router.get('/atme', function(req, res){
-	res.render('myinfos', {url: req.url, type: 'atme'});
+	res.render('myinfos', {url: req.originalUrl, type: 'atme'});
 });
 
 router.get('/friends', function(req, res){
-	res.render('myinfos', {url: req.url, type: 'friends'});
+	res.render('myinfos', {url: req.originalUrl, type: 'friends'});
 });
 
 module.exports = router;
